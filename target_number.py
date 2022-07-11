f950e059c96d52cdb9909d3563d9be81025b6279
JuYeong98 <twinjuy@naver.com>
2022-07-11 12:55:46 +0900

    commit

--- target_number.py
+++ target_number.py
@@ -19,3 +19,21 @@ def solution(numbers, target):
             dfs(idx+1, result-numbers[idx])
     dfs(0,0)
     return answer
+
+from collections import deque
+def solution(numbers, target):
+    answer = 0
+    queue = deque()
+    n = len(numbers)
+    queue.append([numbers[0],0])
+    queue.append([-1*numbers[0],0])
+    while queue:
+        temp, idx = queue.popleft()
+        idx += 1
+        if idx < n:
+            queue.append([temp+numbers[idx], idx])
+            queue.append([temp-numbers[idx], idx])
+        else:
+            if temp == target:
+                answer += 1
+    return answer    
