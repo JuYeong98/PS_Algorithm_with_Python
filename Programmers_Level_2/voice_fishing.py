70beec861b5babd9dd3b91b7de37f799fededf55
JuYeong98 <twinjuy@naver.com>
2022-08-09 22:48:48 +0900

    commit

--- Programmers_Level_2/voice_fishing.py
+++ Programmers_Level_2/voice_fishing.py
@@ -16,12 +16,12 @@ for i in range(len(txt_list)):
             line = line.split(' ')
             for word in line:
                 if len(word) >=2 and len(word)<=5:
-                    """
+                    
                     if len(word) > 2 and word[0:2] in word_list:
                         word_dict[word[0:2]]+=1
                     if len(word) > 3 and word[0:3] in word_list:
                         word_dict[word[0:3]] +=1
-                    """    
+                       
                     if word not in word_list: #단어 리스트에 없는 단어인 경우 단어 리스트에 단어를 추가함
                         word_list.append(word) 
                         word_dict[word] =1 #단어 사전에 1값으로 추가함   
